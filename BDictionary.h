492a9e8cf5aad489bd048b971d3da0965da8a028
Zachary Seeley <zseeley7913@gmail.com>
2025-03-30 19:33:41 -0500

    Finished commenting BDictionary.h

--- BDictionary.h
+++ BDictionary.h
@@ -15,28 +15,29 @@
 template <typename Key, typename E>
 class BDictionary : public Dictionary<Key, E> {
 public:
-	// constructors/destructor
-	BDictionary(size_t size=20)	// Default constructor | default value is arbitrarily chosen
-	{
-		dictionary = new ABag<KVpair<Key, E>>((int)size);
-	}			
-	~BDictionary() { delete dictionary; }			// Base destructor
-	
-	// methods: clear, insert, remove, removeAny, find, size, etc.
+	//Base constructor | default value is arbitrarily chosen
+	//	Create ABag object for dictionary 4member variable using passed in or default size parameter
+	BDictionary(size_t size=20)	{ dictionary = new ABag<KVpair<Key, E>>((int)size); }
+
+	// Base destructor
+	~BDictionary() { delete dictionary; }
 
 	//clear()
 	void clear() { dictionary->emptyBag(); }	//call dictionary's function emptyBag to clear the dictionary
 
 	//insert()
+	//	insert passed in values to dictionary using dictionary->addItem()
+	//	return the true/false value based on the success of the function call
 	bool insert(const Key& k, const E& e) { return dictionary->addItem(KVpair<Key, E>(k, e)); }
 
 	//remove()
+	//	remove a dictionary entry based on passed in parameter values
 	bool remove(const Key& k, E& rtnVal)
 	{
-		KVpair<Key, E> kvPair(k, rtnVal);
-		if (dictionary->remove(kvPair))
+		KVpair<Key, E> kvPair(k, rtnVal);	//Create KVpair object to pass to dictionary->remove()
+		if (dictionary->remove(kvPair))		//if dictionary->remove() is successful...
 		{
-			rtnVal = kvPair.value();
+			rtnVal = kvPair.value();		//set rtnVal function parameter to deleted KVpair value
 			return true;
 		}
 
@@ -44,12 +45,15 @@ public:
 	}
 
 	//removeAny()
+	//	remove an arbitrarily chosen dictionary entry
+	//		the top (most recently added and existing) entry is chosen
+	//		simply because the top value can and is an arbitrary item
 	bool removeAny(E& returnValue)
 	{
-		KVpair<Key, E> kvPair;
-		if (dictionary->removeTop(kvPair))
+		KVpair<Key, E> kvPair;				//Create KVpair object to pass to dictionary->removeTop()
+		if (dictionary->removeTop(kvPair))	//if dictionary->removeTop() is successful...
 		{
-			returnValue = kvPair.value();
+			returnValue = kvPair.value();	//set returnValue function parameter to deleted KVpair value
 			return true;
 		}
 
@@ -57,12 +61,13 @@ public:
 	}
 
 	//find()
+	//	find dictionary entry based on passed in parameter values
 	bool find(const Key& k, E& returnValue) const
 	{
-		KVpair<Key, E> kvPair(k, returnValue);
-		if (dictionary->find(kvPair))
+		KVpair<Key, E> kvPair(k, returnValue);	//Create KVpair object to pass to dictionary->find()
+		if (dictionary->find(kvPair))			//if dictionary->find() is successful...
 		{
-			returnValue = kvPair.value();
+			returnValue = kvPair.value();		//set returnValue function parameter to deleted KVpair value
 			return true;
 		}
 
@@ -70,7 +75,8 @@ public:
 	}
 
 	//size()
-	int size() { return dictionary->size(); }
+	//	return the size of the dictionary
+	int size() { return dictionary->size(); }	//return the value given by the dictionary->size() function
 
 private:
 	//Pointer to a ABag object.  You'll need to instantiate the bag in your constructor:
